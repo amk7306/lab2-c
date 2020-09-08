358600f252350c76f8f8e68a4abf01fa4bbcf28a
Abbey Kato <amk7306@psu.edu>
2020-09-08 14:00:52 +0000

    \n

--- main.c
+++ main.c
@@ -11,34 +11,34 @@
 int main(void) {
   double  grade = atof(readline("Enter your CMPSC 131 grade: "));
   if(grade<60){
-    printf("Your letter grade for CMPSC 131 is F.");
+    printf("Your letter grade for CMPSC 131 is F.\n");
   }
   if(60<grade && grade<=70){
-    printf("Your letter grade for CMPSC 131 is D.");
+    printf("Your letter grade for CMPSC 131 is D.\n");
   }
   if(70<grade && grade<=77){
-    printf("Your letter grade for CMPSC 131 is C.");
+    printf("Your letter grade for CMPSC 131 is C.\n");
   }
   if(77<grade && grade<=80){
-    printf("Your letter grade for CMPSC 131 is C+.");
+    printf("Your letter grade for CMPSC 131 is C+.\n");
   }
   if(80<grade && grade<=83){
-    printf("Your letter grade for CMPSC 131 is B-.");
+    printf("Your letter grade for CMPSC 131 is B-.\n");
   }
   if(83<grade && grade<=87){
-    printf("Your letter grade for CMPSC 131 is B.");
+    printf("Your letter grade for CMPSC 131 is B.\n");
   }
   if(87<grade && grade<=90){
-    printf("Your letter grade for CMPSC 131 is B+.");
+    printf("Your letter grade for CMPSC 131 is B+.\n");
   }
   if(90<grade && grade<93){
-    printf("Your letter grade for CMPSC 131 is A-.");
+    printf("Your letter grade for CMPSC 131 is A-.\n");
   }
   if(93<=grade && grade<100){
-    printf("Your letter grade for CMPSC 131 is A");
+    printf("Your letter grade for CMPSC 131 is A.\n");
   }
   if(grade==100){
-    printf("Your letter grade for CMPSC 131 is A+");
+    printf("Your letter grade for CMPSC 131 is A+.\n");
   }
   return 0;
 }
